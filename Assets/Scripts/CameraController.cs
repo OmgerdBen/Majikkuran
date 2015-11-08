3a32ed1b7ba7b5aa0afc2648b37cead12bc8e8ef
cgallarin <cgallari@emich.edu>
2015-11-08 03:25:44 -0500

    Prefabs, Character and Camera Controllers
    Prefabs: Wizard, Fruit(2), Tree, Rock. Character controller allows the
    player to move continuously through level and jumps when the space bar
    is pressed. Camera controller follows the player while keeping a steady
    shot of screen.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public PlayerController thePlayer;      // location of player
+
+    private Vector3 lastPlayerPosition;     // x, y, z position of player
+    private float distanceToMove;
+
+	// Use this for initialization
+	void Start () {
+        thePlayer = FindObjectOfType<PlayerController>();
+        lastPlayerPosition = thePlayer.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // distance we want camera to move (x value current position - x value last position)
+        distanceToMove = thePlayer.transform.position.x - lastPlayerPosition.x;
+
+        // do not change y and z value (so if the player jumps, the camera will not move up and down)
+        transform.position = new Vector3(transform.position.x + distanceToMove, transform.position.y, transform.position.z);
+
+        lastPlayerPosition = thePlayer.transform.position;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public bool grounded;
+    public LayerMask whatIsGround;
+    public float moveSpeed;
+    public float jumpForce;
+
+    private Rigidbody2D myRigidBody;
+    private Collider2D myCollider;
+
+	// Use this for initialization
+	void Start () {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myCollider = GetComponent<Collider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // is the player on the ground?!
+        // returns true if collider is touching another collider
+        grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
+
+        // do not change y value
+        myRigidBody.velocity = new Vector2(moveSpeed, myRigidBody.velocity.y);
+
+        // when space bar is pressed, character jumps
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // player can only jump if on ground
+            if (grounded)
+            {
+                // do not change x value
+                myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, jumpForce);
+            }
+        }
+	}
+}
